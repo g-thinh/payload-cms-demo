18d11a59718c083b89162951f9353d616d8c9330
Gia Thinh Nguyen <thinh-nguyen@hotmail.com>
2023-07-22 21:44:55 -0400

    update env vars

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,10 +5,18 @@ import { Users } from "./collections/Users";
 import { Examples } from "./collections/Examples";
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_URL,
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   admin: {
     user: Admins.slug,
   },
+  cors: [
+    process.env.PAYLOAD_PUBLIC_SERVER_URL || "",
+    process.env.PAYLOAD_PUBLIC_SITE_URL || "",
+  ].filter(Boolean),
+  csrf: [
+    process.env.PAYLOAD_PUBLIC_SERVER_URL || "",
+    process.env.PAYLOAD_PUBLIC_SITE_URL || "",
+  ].filter(Boolean),
   collections: [Admins, Users, Examples],
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
